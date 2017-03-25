96ba1b9321dd7003d806a2baccf1bbca46e1d027
Elena Esina <hellemess@yandex.ru>
2017-03-25 22:19:50 +0300

    сверстала шапку и заголовок главной страницы для трех состояний: мобильного, планшетного и десктопного

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+window.script = (function () {
+  var mainNav = document.querySelector('.main-header__main-nav');
+  var menuToggle = mainNav.querySelector('.main-header__menu-toggle');
+
+  mainNav.classList.remove('main-header__main-nav--no-js');
+
+  menuToggle.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mainNav.classList.toggle('main-header__main-nav--closed');
+    mainNav.classList.toggle('main-header__main-nav--opened');
+  });
+})();

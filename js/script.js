661eea099ea30ba978e42a3ee8ff9d283ec98b63
Elena Esina <hellemess@yandex.ru>
2017-03-26 19:42:10 +0300

    сверстала диалоговое окно и задала его поведение

--- js/script.js
+++ js/script.js
@@ -11,4 +11,18 @@ window.script = (function () {
     mainNav.classList.toggle('main-header__main-nav--closed');
     mainNav.classList.toggle('main-header__main-nav--opened');
   });
+
+  var orderBtn = document.querySelector('.product-of-week .btn');
+  var dialog = document.querySelector('.main-content__dialog');
+
+  orderBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    dialog.classList.add('main-content__dialog--show');
+
+    document.addEventListener('keydown', function (orderEvt) {
+      if (orderEvt.keyCode && orderEvt.keyCode === 27) {
+        dialog.classList.remove('main-content__dialog--show');
+      }
+    });
+  });
 })();

7482446caae73c84d5c4b4fbbc050ecd63a04fe6
Elena Esina <hellemess@yandex.ru>
2017-03-28 18:41:57 +0300

    сверстала карточки товаров на странице каталога

--- js/dialog.js
+++ js/dialog.js
@@ -1,17 +1,6 @@
 'use strict';
 
-window.script = (function () {
-  var mainNav = document.querySelector('.main-header__main-nav');
-  var menuToggle = mainNav.querySelector('.main-header__menu-toggle');
-
-  mainNav.classList.remove('main-header__main-nav--no-js');
-
-  menuToggle.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    mainNav.classList.toggle('main-header__main-nav--closed');
-    mainNav.classList.toggle('main-header__main-nav--opened');
-  });
-
+window.dialog = (function () {
   var orderBtn = document.querySelector('.product-of-week .btn');
   var dialog = document.querySelector('.main-content__dialog');
 

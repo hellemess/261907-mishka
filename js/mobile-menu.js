0b12d435258974a364f732cb8e5346a2e501641d
Elena Esina <hellemess@yandex.ru>
2017-03-29 13:49:57 +0300

    доверстала страницу каталога

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,14 @@
 'use strict';
 
-if (document.documentElement.clientWidth < 768) {
-  window.mobileMenu = (function () {
-    var mainNav = document.querySelector('.main-header__main-nav');
-    var menuToggle = mainNav.querySelector('.main-header__menu-toggle');
+window.mobileMenu = (function () {
+  var mainNav = document.querySelector('.main-header__main-nav');
+  var menuToggle = mainNav.querySelector('.main-header__menu-toggle');
 
-    mainNav.classList.remove('main-header__main-nav--no-js');
+  mainNav.classList.remove('main-header__main-nav--no-js');
 
-    menuToggle.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      mainNav.classList.toggle('main-header__main-nav--closed');
-      mainNav.classList.toggle('main-header__main-nav--opened');
-    });
-  })();
-}
+  menuToggle.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mainNav.classList.toggle('main-header__main-nav--closed');
+    mainNav.classList.toggle('main-header__main-nav--opened');
+  });
+})();

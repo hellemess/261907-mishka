bb1248f55d8bf795d97b80b50f41e1c83ee02e99
Elena Esina <hellemess@yandex.ru>
2017-04-01 19:40:21 +0300

    добавила графику на страницу каталога, настроила открытие диалогового окна по нажатию на иконку корзины

--- js/dialog.js
+++ js/dialog.js
@@ -1,17 +1,45 @@
 'use strict';
 
 window.dialog = (function () {
-  var orderBtn = document.querySelector('.product-of-week .btn');
   var dialog = document.querySelector('.main-content__dialog');
+  var orderBtn = document.querySelector('.product-of-week .btn');
+  var productCards = document.querySelector('.main-content__container');
 
-  orderBtn.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    dialog.classList.add('main-content__dialog--show');
+  var closeDialog = function () {
+    dialog.classList.remove('main-content__dialog--show');
+    dialog.removeEventListener('click', onClick);
+    document.removeEventListener('keydown', onEscDown);
+  }
 
-    document.addEventListener('keydown', function (orderEvt) {
-      if (orderEvt.keyCode && orderEvt.keyCode === 27) {
-        dialog.classList.remove('main-content__dialog--show');
+  var onClick = function (evt) {
+    if (evt.target.classList.contains('main-content__dialog')) {
+      closeDialog();
+    }
+  };
+
+  var onEscDown =  function (evt) {
+    if (evt.keyCode && evt.keyCode === 27) {
+      closeDialog();
+    }
+  };
+
+  if (orderBtn !== null) {
+    orderBtn.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      dialog.classList.add('main-content__dialog--show');
+      dialog.addEventListener('click', onClick);
+      document.addEventListener('keydown', onEscDown);
+    });
+  }
+
+  if (productCards !== null) {
+    productCards.addEventListener('click', function (evt) {
+      if (evt.target.classList.contains('product__cart') || evt.target.tagName === 'svg' || evt.target.tagName === 'use') {
+        evt.preventDefault();
+        dialog.classList.add('main-content__dialog--show');
+        dialog.addEventListener('click', onClick);
+        document.addEventListener('keydown', onEscDown);
       }
     });
-  });
+  }
 })();

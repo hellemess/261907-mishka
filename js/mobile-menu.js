5a08fb1fe3af769779b19d39249bbf09507afbff
Elena Esina <hellemess@yandex.ru>
2017-04-03 00:26:08 +0300

    написала автоматическую сборку личного проекта

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 window.mobileMenu = (function () {
-  var mainNav = document.querySelector('.main-header__main-nav');
-  var menuToggle = mainNav.querySelector('.main-header__menu-toggle');
+  var mainNav = document.querySelector(".main-header__main-nav");
+  var menuToggle = mainNav.querySelector(".main-header__menu-toggle");
 
-  mainNav.classList.remove('main-header__main-nav--no-js');
+  mainNav.classList.remove("main-header__main-nav--no-js");
 
-  menuToggle.addEventListener('click', function (evt) {
+  menuToggle.addEventListener("click", function (evt) {
     evt.preventDefault();
-    mainNav.classList.toggle('main-header__main-nav--closed');
-    mainNav.classList.toggle('main-header__main-nav--opened');
+    mainNav.classList.toggle("main-header__main-nav--closed");
+    mainNav.classList.toggle("main-header__main-nav--opened");
   });
 })();
